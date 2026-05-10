12b6ad13499c3de97de883ab5300597dfed172bc
mikhailfur <79972291+mikhailfur@users.noreply.github.com>
2026-05-11 03:37:57 +0900

    clearance

--- backend/src/health.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common'
-
-@Controller()
-export class HealthController {
-  @Get('health')
-  healthCheck() {
-    return { status: 'ok', timestamp: new Date().toISOString() }
-  }
-}
